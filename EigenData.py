824c631a76a6e4c2cafa8489e0b1aa5e6b7afc0b
Angeliki <sc20af@leeds.ac.uk>
2023-02-24 16:58:04 +0000

    Added code that implements visual representation of the worm shape for each of the 12 worms

--- EigenData.py
+++ EigenData.py
@@ -1,6 +1,6 @@
 # Initial code taken from stephens-2d-eigenworm-data gitlab repository
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io
 import h5py
@@ -12,6 +12,13 @@ class EigenData(object):
     def __init__(self, eigen_path ="", footage_path=""):
         self._eigen_path = eigen_path
         self._footage_path = footage_path
+    
+    def generate_postures(self, array1, array2):
+        plt.plot(array1, array2, '.')
+        plt.title("Worm Shape")
+        plt.xlabel("s")
+        plt.ylabel("θ")
+        plt.show()
 
     def get_data(self):
         '''
@@ -20,9 +27,10 @@ class EigenData(object):
         '''
         #Load the matlab files into numpy arrays
         eigenworms = scipy.io.loadmat(self._eigen_path)
-        eigenworms = eigenworms["EigenWorms"].transpose()
+        eigenworms = eigenworms["EigenWorms"]
         footage = {}
-        f = h5py.File(self._footage_path)
+        f = h5py.File(self._footage_path, "r+")
+        eigenworms = eigenworms[:, :6]
         i = 0
         for k, v in f.items():
             if k != "tr":
@@ -30,4 +38,42 @@ class EigenData(object):
                     if v2.shape == (5, 33600) or v2.shape == (6, 33600):
                         footage[i] = np.array(v2)
                         i+=1
+    #multiplication of the eigenvalues with the eigenvectors for each worm to get tje arc length θ(s)
+    #worm_array is a list that contatins the arc length for the points of one worm
+    #big_array is a list that contains 12 lists (one for each worm used) that contain the arc length of the first 100 points
+        wm = 0
+        worm_array = []
+        big_array = []
+        for wormfootage in footage:
+            worm = footage[wormfootage]
+            worm = worm.transpose()
+            # print(worm)
+            wmrow_cnt = 0
+            for wormRow in worm[:100]:
+                # print(wormRow)
+                # print(wmrow_cnt)
+                value_cnt = 0
+                for wormValue in wormRow:
+                    wm += wormValue*eigenworms[wmrow_cnt][value_cnt]
+                    value_cnt += 1
+                worm_array.append(wm)
+                wmrow_cnt += 1
+                wm = 0
+            big_array.append(worm_array)
+            worm_array = []
+        # print(big_array)
+        numbers_array = []
+
+        for num in range(1, 101):
+            numbers_array.append(num/100)
+            
+            
+        for i in range(0, 12):
+            self.generate_postures(numbers_array, big_array[i])
+            
+            
+            
         return eigenworms, footage
+
+ed = EigenData("EigenWorms.mat", "20150814-All-PNAS2011-DataStitched .mat")
+d1, d2 = ed.get_data()
