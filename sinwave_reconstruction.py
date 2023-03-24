e7c0ac4cbd8b52f58cf083bb91038994e507b9f6
Angeliki <sc20af@leeds.ac.uk>
2023-03-24 23:44:35 +0000

    Plot heatmaps

--- sinwave_reconstruction.py
+++ sinwave_reconstruction.py
@@ -10,20 +10,23 @@ def generate_matrix():
     #construct covariance matrix
     cov_matrix = angles[np.arange(100)[:, None] - np.arange(100)]
     return angles,cov_matrix
+
 def eigen_decomposition(matrix):
     #get the eigenvalues and eigenvectors from the covariance matrix
     eigenvalues, eigenvectors = np.linalg.eig(matrix)
     return eigenvalues, eigenvectors
+
 def sort_eigen(eigenvalues, eigenvectors):
     # Sort eigenvalues and eigenvectors
     sort_indices = np.argsort(eigenvalues)[::-1]
     sorted_eigenvalues = eigenvalues[sort_indices]
     sorted_eigenvectors = eigenvectors[:, sort_indices]
     return sorted_eigenvalues,sorted_eigenvectors
+
 def choose_k_eigenvectors(k,eigenvalues,eigenvectors):
     eigenvalues_selected = eigenvalues[:k]
     eigenvectors_selected = eigenvectors[:, :k]
-    return eigenvalues_selected,
+    return eigenvalues_selected,eigenvectors_selected
 def reconstructed_matrix(k, eigenvalues,eigenvectors,matrix):
     reconstructed_matrix = np.zeros_like(matrix, dtype=np.complex128)
     for i in range(k):
@@ -42,7 +45,6 @@ def plot_matrix_vs_reconstructed_matrix(reconstructed_matrix,angles):
     axis[1].plot(angles, x_recon[:, 0], label='Reconstructed Matrix')
     axis[1].set_title("Reconstructed Matrix")
     plt.show()
-
 def plot_histograms(reconstructed_matrix,cov_matrix):
     fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(10, 5))
     im1 = ax1.imshow(cov_matrix)
@@ -52,7 +54,17 @@ def plot_histograms(reconstructed_matrix,cov_matrix):
     fig.colorbar(im1, ax=ax1)
     fig.colorbar(im2, ax=ax2)
     plt.show()
+def plot_error_histogram(reconstructed_matrix,cov_matrix):
+    error = cov_matrix - reconstructed_matrix
+    # Plot the heatmap
+    plt.imshow(error, cmap='viridis', interpolation='nearest')
+    # Add colorbar
+    plt.colorbar()
+    # Show the plot
+    plt.show()
+
 def main():
+    
     angles,covariance_matrix = generate_matrix()
     eigenvalues, eigenvectors = eigen_decomposition(covariance_matrix)
     eigenvalues, eigenvectors = sort_eigen(eigenvalues, eigenvectors)
@@ -61,5 +73,8 @@ def main():
     recon_matrix = reconstructed_matrix(k,eigenvalues_selected,eigenvectors_selected,covariance_matrix)
     plot_matrix_vs_reconstructed_matrix(recon_matrix,angles)
     plot_histograms(recon_matrix,covariance_matrix)
+    plot_error_histogram(recon_matrix,covariance_matrix)
+
+
 if __name__ == "__main__":
     main()
