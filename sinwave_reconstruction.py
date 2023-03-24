78579bf3b8612d8700c6b7214a6a646a38d26337
Angeliki <sc20af@leeds.ac.uk>
2023-03-24 23:34:05 +0000

    Generate matrix

---
+++ sinwave_reconstruction.py
@@ -0,0 +1,20 @@
+#libraries needed to generate and reconstruct using eigenvectors and eigenvalues the sin wave
+import numpy as np
+from scipy.linalg import toeplitz
+import matplotlib.pyplot as plt
+
+def generate_matrix():
+    angles = np.linspace(0, 2 * np.pi, num=100, endpoint=False)
+    #This will create an array called angles with 100 equally spaced angles between 0 and 2π,
+    #where the endpoint=False argument means that the end point 2π is not included in the angles
+    #construct covariance matrix
+    cov_matrix = angles[np.arange(100)[:, None] - np.arange(100)]
+    return angles,cov_matrix
+
+def main():
+    angles,covariance_matrix = generate_matrix()
+   
+
+
+if __name__ == "__main__":
+    main()
